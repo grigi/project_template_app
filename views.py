833958d1da02a3130a9e963b14710b09128c1514
Grigi <nagrigoriadis@gmail.com>
2012-08-04 22:14:11 +0200

    Added Jinja2 and HAML stuff. Also made docstring of sample test one line.

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 # Views for app '{{ app_name }}'
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+# For using Jinja2 templates
+#from coffin.shortcuts import render_to_response
+#from coffin.template import RequestContext
 
 # Create your views here.
 
